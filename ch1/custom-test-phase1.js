73ebbc8fd963e0f7fb2fcbd0ac1c6af6f5bd9f8b
Eli Sherer <eli.sherer@gmail.com>
2019-08-04 23:02:55 +0300

    Update custom-test-phase1.js
    no reason using let if those variables don't change, missing backticks

--- ch1/custom-test-phase1.js
+++ ch1/custom-test-phase1.js
@@ -1,18 +1,18 @@
 //I'm using 'require' to be compatible with node.js,
 // so we won't need a transpiler like babel.
-let NumberParser = require('./number-parser');
+const NumberParser = require('./number-parser');
 
 /**
  * Our "Test" definition.
  */
 const parserTest = () => {
   try {
-    let np = new NumberParser();
-    let result = np.sum("1,2");
+    const np = new NumberParser();
+    const result = np.sum("1,2");
     if (result === 3) {
       console.log("parserTest example 1 PASSED");
     } else {
-      throw new Error("parserTest: expected 3 but was ${result}");
+      throw new Error(`parserTest: expected 3 but was ${result}`);
     }
   } catch (e) {
     console.error(e.stack);
